1c65b7ae6649724246dd924416c0d8f58ffeac70
Thai Dang <thai.dang.fr@gmail.com>
2019-03-04 16:02:57 +0100

    update

---
+++ install_ruby.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+usage() {
+  echo "Usage $0 -u <ruby user>" 1>&2
+} 
+
+while getopts ":u:h" opt; do
+  case $opt in
+    u)
+      echo "-u was triggered, Parameter: $OPTARG" >&2
+      export RUBYUSR=$OPTARG
+      ;;
+    h)
+      usage
+      exit 1
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
+
+ansible-playbook -e "username=$RUBYUSR" install_ruby.yml
