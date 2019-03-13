cde0705a755ccd247b82b6f2feba4992f9c02837
Thai Dang <thai.dang.fr@gmail.com>
2019-03-13 18:02:18 +0100

    update

--- install_rails.sh
+++ install_rails.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 usage() {
-  echo "Usage $0 -g <github url> -s <server name>" 1>&2
+  echo "Usage $0 -g <github url> -s <server name> -v <ruby version>" 1>&2
 } 
 
-while getopts ":g:s:h" opt; do
+while getopts ":g:s:v:h" opt; do
   case $opt in
     g)
       echo "-g was triggered, Parameter: $OPTARG" >&2
@@ -13,6 +13,10 @@ while getopts ":g:s:h" opt; do
     s)
       echo "-s was triggered, Parameter: $OPTARG" >&2
       export SERVERNAME=$OPTARG
+      ;;
+    v)
+      echo "-v was triggered, Parameter: $OPTARG" >&2
+      export RUBYVERSION=$OPTARG
       ;;
     h)
       usage
@@ -29,7 +33,7 @@ while getopts ":g:s:h" opt; do
   esac
 done
 
-if [ $# -lt 4 ];then
+if [ $# -lt 6 ];then
     usage
     exit 1
 fi
@@ -37,4 +41,4 @@ fi
 export PROJECT=$(basename $GITHUB .git | awk '{print tolower($0)}')
 export RAILSUSR=$PROJECT
 
-ansible-playbook -e "username=$RAILSUSR github=$GITHUB project=$PROJECT servername=$SERVERNAME" install_rails.yml
+ansible-playbook -e "username=$RAILSUSR github=$GITHUB project=$PROJECT servername=$SERVERNAME ruby_version=$RUBYVERSION" install_rails.yml
--- install_ruby.sh
+++ install_ruby.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 usage() {
-  echo "Usage $0 -u <ruby user>" 1>&2
+  echo "Usage $0 -u <ruby user> -v <ruby version>" 1>&2
 } 
 
-while getopts ":u:h" opt; do
+while getopts ":u:v:h" opt; do
   case $opt in
     u)
       echo "-u was triggered, Parameter: $OPTARG" >&2
       export RUBYUSR=$OPTARG
+      ;;
+    v)
+      echo "-v was triggered, Parameter: $OPTARG" >&2
+      export RUBYVERSION=$OPTARG
       ;;
     h)
       usage
@@ -25,9 +29,9 @@ while getopts ":u:h" opt; do
   esac
 done
 
-if [ $# -lt 1 ];then
+if [ $# -lt 2 ];then
     usage
     exit 1
 fi
 
-ansible-playbook -e "username=$RUBYUSR" install_ruby.yml
+ansible-playbook -e "username=$RUBYUSR ruby_version=$RUBYVERSION" install_ruby.yml
