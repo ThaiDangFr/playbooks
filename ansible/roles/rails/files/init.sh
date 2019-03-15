f3cb89064983aeea2fc770d535d17492b7a617d0
Thai Dang <thai.dang.fr@gmail.com>
2019-03-15 11:34:14 +0100

    stable

--- ansible/roles/rails/files/init.sh
+++ ansible/roles/rails/files/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 
 LOG_FILE=~/code/log/init-project.log
-echo "$(date)"> $LOG_FILE 2>&1
+echo "$(date)">> $LOG_FILE 2>&1
 
 export RAILS_ENV=production 
 
@@ -9,4 +9,5 @@ bundle install --deployment --without development test >> $LOG_FILE 2>&1
 bundle exec rake assets:precompile db:migrate >> $LOG_FILE 2>&1
 chmod 700 config db >> $LOG_FILE 2>&1
 chmod 600 config/database.yml config/secrets.yml >> $LOG_FILE 2>&1
+echo >> $LOG_FILE 2>&1
 #bundle exec passenger-config restart-app .
