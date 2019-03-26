db0cfda862224d3d3dece6fc9c6e05aaa5daf5bd
Thai Dang <thai.dang.fr@gmail.com>
2019-03-26 17:36:29 +0100

    stable

---
+++ install_etherpad.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+usage() {
+  echo "Usage $0 -u <admin username> -p <admin password>" 1>&2
+}
+
+while getopts ":u:p:h" opt; do
+  case $opt in
+    u)
+      echo "-u was triggered, Parameter: $OPTARG" >&2
+      export AUSERNAME=$OPTARG
+      ;;
+    p)
+      echo "-p was triggered, Parameter: $OPTARG" >&2
+      export APASSWORD=$OPTARG
+      ;;
+    h)
+      usage
+      exit 1
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
+if [ $# -lt 1 ];then
+    usage
+    exit 1
+fi
+
+export scriptpath=$(dirname $0)
+cd ${scriptpath}/ansible
+ansible-playbook -e "username=$AUSERNAME password=$APASSWORD" install_etherpad.yml
