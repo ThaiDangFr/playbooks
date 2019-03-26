9966ba8b6afcf42c7ded701810cebcd42f9fdad8
Thai Dang <thai.dang.fr@gmail.com>
2019-03-26 18:01:48 +0100

    stable

--- install_etherpad.sh
+++ install_etherpad.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 usage() {
-  echo "Usage $0 -u <admin username> -p <admin password>" 1>&2
+  echo "Usage $0 -u <admin username> -p <admin password> -s <server name>" 1>&2
 }
 
-while getopts ":u:p:h" opt; do
+while getopts ":u:p:s:h" opt; do
   case $opt in
     u)
       echo "-u was triggered, Parameter: $OPTARG" >&2
@@ -13,6 +13,10 @@ while getopts ":u:p:h" opt; do
     p)
       echo "-p was triggered, Parameter: $OPTARG" >&2
       export APASSWORD=$OPTARG
+      ;;
+    s)
+      echo "-s was triggered, Parameter: $OPTARG" >&2
+      export SERVERNAME=$OPTARG
       ;;
     h)
       usage
@@ -36,4 +40,4 @@ fi
 
 export scriptpath=$(dirname $0)
 cd ${scriptpath}/ansible
-ansible-playbook -e "username=$AUSERNAME password=$APASSWORD" install_etherpad.yml
+ansible-playbook -e "username=$AUSERNAME password=$APASSWORD server_name=$SERVERNAME" install_etherpad.yml
